9d8639bdd845f762c7bc47b2e724e47101067d45
Edleron <Edleron@hotmail.com>
2024-09-24 14:38:23 +0300

    add : collect scirpts

--- Assets/Script/CollectCoins.cs
+++ Assets/Script/CollectCoins.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollectCoins : MonoBehaviour
 {
     public float score;
     public TextMeshProUGUI CoinText;
+    public PlayerController playerController;
+    private int maxScore = 20;
+    public Animator animator;
+    public GameObject player;
+
+    private void Start()
+    {
+        animator = player.GetComponentInChildren<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +25,19 @@ public class CollectCoins : MonoBehaviour
             AddCoin();
             Destroy(other.gameObject);
         }
+        else if (other.CompareTag("Finished"))
+        {
+            playerController.runningSpeed = 0;
+            transform.Rotate(transform.rotation.x, 180, transform.rotation.z, Space.Self);
+            if (score >= maxScore)
+            {
+                animator.SetBool("Win", true);
+            }
+            else
+            {
+                animator.SetBool("Lose", true);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,6 +45,7 @@ public class CollectCoins : MonoBehaviour
         if (collision.collider.CompareTag("Obstacle"))
         {
             Debug.Log("TEST");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
